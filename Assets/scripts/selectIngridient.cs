db61ece1447e23e831e49bc39176d1609495eddc
hatdog42 <kristian.lillesand@gmail.com>
2023-12-01 21:01:17 +0100

    selectstuf

--- Assets/scripts/selectIngridient.cs
+++ Assets/scripts/selectIngridient.cs
@@ -1,12 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class selectIngridient : MonoBehaviour
 {
-    
     public string name;
+    private Material _material;
+    private SpriteRenderer spriteRenderer;
+    public List<string> ingridients; 
     
+    
+    private bool selekted;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        ingridients = GameObject.Find("SelectControler").GetComponent<selectcontroler>().ingridients;
+        
+        _material = new Material(spriteRenderer.material);
+
+        spriteRenderer.material = _material;
+
+        name = gameObject.name;
+    }
+
     private void OnMouseDown()
     {
+        if (!selekted)
+        {
+            if (selectcontroler.selectConter <5)
+            {
+                selekted = true;
+                selectcontroler.selectConter++;
+                
+                ingridients.Add(name);
+                
+                print(name+"selected");
+            }
+            
+        }
+        else
+        {
+            selekted = false;
+
+            selectcontroler.selectConter--;
+            
+        }
         
     }
+    
 }
--- Assets/scripts/selectcontroler.cs
+++ Assets/scripts/selectcontroler.cs
@@ -1,18 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class selectcontroler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int selectConter;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<string> ingridients;
+
 }
