721d7e3561685910baa3fed0d86d2547d38d5113
Benjameepmeep <benjamin.arctander@gmail.com>
2023-12-01 20:12:51 +0100

    Score Script

---
+++ Assets/scripts/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Score >= 30)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
